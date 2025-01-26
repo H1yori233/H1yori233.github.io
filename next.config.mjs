2685caf5eb4588e44ce45477a12c27e7be062ebb
H1yori233 <hiyoriizumi233@gmail.com>
2025-01-26 13:51:10 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: '/H1yori233.github.io/',
-  basePath: '/H1yori233.github.io',
   images: {
     unoptimized: true,
   },
@@ -25,28 +16,6 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-}
-
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
 }
 
 export default nextConfig
