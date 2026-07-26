ffbc4bfdaa39d6e0882df788eed0c6c2e6fcb635
Kaiqin Kong <hiyoriizumi233@gmail.com>
2026-07-25 22:52:49 -0700

    Refactor command registration and interfaces

--- lib/os/commands/index.ts
+++ lib/os/commands/index.ts
@@ -160,6 +160,6 @@ registerCommand({
     name: 'cv',
     description: 'Open my CV',
     execute: async (_args, _stdin, _shell) => {
-        return { output: '\x1b[LINK]/pdfs/cv.pdf|Kaiqin\'s CV\x1b[/LINK]' };
+        return { output: '\x1b[LINK]/cv.pdf|Kaiqin\'s CV\x1b[/LINK]' };
     }
 });
